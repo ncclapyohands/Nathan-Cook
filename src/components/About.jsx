a3f8a3a24f79f8f7a454b196f87b88910998c546
ncclapyohands <nathancook648@gmail.com>
2024-03-05 02:21:21 -0700

    Idk why this is happening

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import "../styling/about.css"; // Add your styles in AboutMe.css
 import Teton7 from "../assets/teton7.jpeg";
-import r2 from "../assets/4r2.jpeg";
-import r3 from "../assets/4r3.jpeg";
-import r4 from "../assets/4r4.jpeg";
+import Runner2 from "../assets/4r2.jpeg";
+import Runner3 from "../assets/4r3.jpeg";
+import Runner4 from "../assets/4r4.jpeg";
 import Carter from "../assets/carter.jpg";
-import n8b from "../assets/n8b.jpeg";
+import NateAndBent from "../assets/n8b.jpeg";
 import Waterfall from "../assets/waterfall1.jpeg";
-import teton6 from "../assets/teton6.jpeg";
+import Teton6 from "../assets/teton6.jpeg";
 //import PuretoRico from "../assets/puretorico2.jpg";
-import teton8 from "../assets/teton8.jpeg";
-import pr4 from "../assets/pr4.jpeg";
+import Teton8 from "../assets/teton8.jpeg";
+import PuertoRico4 from "../assets/pr4.jpeg";
 
 const About = () => {
   useEffect(() => {
@@ -51,9 +51,24 @@ const About = () => {
       <div className="scroll">
         <div id="img-track" className="scroll-inner">
           <img className="image" src={Teton7} alt="Tetons" draggable="false" />
-          <img className="image" src={r2} alt="4runner2" draggable="false" />
-          <img className="image" src={r3} alt="4runner3" draggable="false" />
-          <img className="image" src={r4} alt="4runner4" draggable="false" />
+          <img
+            className="image"
+            src={Runner2}
+            alt="4runner2"
+            draggable="false"
+          />
+          <img
+            className="image"
+            src={Runner3}
+            alt="4runner3"
+            draggable="false"
+          />
+          <img
+            className="image"
+            src={Runner4}
+            alt="4runner4"
+            draggable="false"
+          />
           <img
             className="image"
             src={Carter}
@@ -62,22 +77,22 @@ const About = () => {
           />
           <img
             className="image"
-            src={n8b}
+            src={NateAndBent}
             alt="bentley my cusin"
             draggable="false"
           />
           <img className="image" src={Waterfall} alt="fam" draggable="false" />
-          <img className="image" src={teton6} alt="lincoln" draggable="false" />
+          <img className="image" src={Teton6} alt="lincoln" draggable="false" />
           {/* <img
             className="image"
             src={PuretoRico}
             alt="pr squad"
             draggable="false"
           /> */}
-          <img className="image" src={teton8} alt="Tetons!" draggable="false" />
+          <img className="image" src={Teton8} alt="Tetons!" draggable="false" />
           <img
             className="image"
-            src={pr4}
+            src={PuertoRico4}
             alt="pr squad line-up"
             draggable="false"
           />

4d81e35562b484118ff1b9a2e62b397fa835d178
ncclapyohands <nathancook648@gmail.com>
2024-03-10 21:57:53 -0600

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { Home, ErrorPage } from "./components";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import Aos from "aos";
 
 function App() {
+  useEffect(() => {
+    Aos.init({
+      once: true,
+      disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  });
   const router = createBrowserRouter([
     {
       path: "/",
